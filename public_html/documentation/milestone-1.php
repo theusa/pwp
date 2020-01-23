8810631cf93d0f9a7f810202c176bd1838dc9c8c
ualnaji@cnm.edu <ualnaji@cnm.edu>
2020-01-23 13:21:08 -0700

    added * to vendor on .gitignore

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title> PWP Objective 1</title>
+</head>
+<body>
+<h1> Do Drugs Well </h1>
+<h2>Purpose</h2>
+
+<p> This project will tell people in no uncertain terms why and how people do certain drugs, and how it affects them. </p>
+<p> It will also gently, and stealthily, nudge people towards activities that achieve the same purpose without drug use. </p>
+
+<h2>Audience</h2>
+
+<p> This site is designed for people who either want to take drugs
+    recreationally, but are not sure what effect it may have on them, how to do them, or what others use them for.
+
+</p>
+
+<h2>Goal</h2>
+<p> Most information online today are either how drugs are bad for you, or personal and unprofessional "trip reports".
+    Normalizing the conversation about chemicals we put into our bodies while referencing studies and their history,
+    we aim to lessen the allure, mistique, and potential misuse. This could help many people that may
+    over use, incorrect use, or benefit from certain chemicals. It worked for pot, it could work for everything else.
+    Also references the podcast I would be recording about the same topic which in this imaginary world I'd have enough focus to shoot/record.
+
+</p>
+
+<h2>Persona</h2>
+
+
+    <p><strong>Name: </strong>Molly Mccrackencrackle</p>
+    <p><strong>Age: </strong>30</p>
+
+    <p><strong>Profession</strong></p>
+        <p>
+            Molly Mccrackencrackle works as a software engineer at an Albuquerque Dev-Shop.
+            She's heard about developers in Silicon Valley taking mushrooms and Adderall to improve their performance inside and outside of her work.
+            She's also felt he attention span was much shorter than those around her, but is taking Adderall right for her? I remember people taking it in college to study for tests, but I'm not sure how I feel about it as a daily drug.
+            She also feels some distance between her and her boyfriend and isn't sure if she wants to stay with him.
+            After all she's 30 and in her sexual, reproductive, and working prime. Does she want to "waste" that with Joe Average?
+            She's read about DMT trips that were life changing for some people, but is tripping balls the right answer for her? Or is taking that Xanex bar and staying with Joe Average the right call?
+            Or are there other options that she isn't even aware of?
+        </p>
+
+    <p><strong>Technology</strong></p>
+        <p>
+            Macbook Pro. Connects to a screen to code at the office. Heavy iPhone 11 user with unlimited data.
+        </p>
+
+    <p><strong>Attitudes and Behaviors</strong></p>
+
+        <p>
+            Feels comfortable with her life, but starting to think maybe there's something more to it. This can't be it!
+            At the same time, what if I'm acting spoiled? After all I have a good relationship with my parents, a job that pays me OK, a decent spouse. What else can anyone ask for?
+
+        </p>
+
+    <p><strong>Frustrations and Needs</strong></p>
+
+        <p>
+            Maybe Molly Mccrackencrackle is feeling too comfortable. She's frustrated in her indecisiveness.
+            Should I spend 3 hours every night to work on my skills and apply for jobs at Google? Or should I go out drinking with my friends?
+            Should I stay with Joe Average or leave him in hopes for someone better?
+        </p>
+
+
+    <p><strong>Goals</strong></p>
+
+        <p>
+            Molly Mccrackencrackle wants to know if drugs are the answer for her.
+            She also wants to know how to safely do them, where to safely obtain them, and discover new drugs that could potentially help her more than the ones she's already heard about.
+        </p>
+
+
+    <p><strong>User Story</strong></p>
+
+        <p>
+            As a user I want to explore how different chemicals affect my body in a safe and professional way.
+            But also maybe exogenous chemicals aren't the answer. I want to figure that out.
+        </p>
+
+
+
+</body>
+</html>
